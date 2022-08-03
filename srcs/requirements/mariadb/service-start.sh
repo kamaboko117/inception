9bda6c2a1d64eedfc50ac2152d4722b7f3e93383
Alexander SABOURET <asaboure@e2r7p11.clusters.42paris.fr>
2022-08-03 18:58:31 +0200

    added container depend condition to docker compose

--- srcs/requirements/mariadb/service-start.sh
+++ srcs/requirements/mariadb/service-start.sh
@@ -3,6 +3,3 @@ service mysql start
 echo "GRANT ALL ON *.* TO 'kama'@'localhost' IDENTIFIED BY '123'" | mysql -u root
 echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
 echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
-
-# exec makes it start as PID 1 
-exec mysqld
