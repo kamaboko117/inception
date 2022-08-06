8a5fad1775e628c726a005dcf2a362cf105a13a1
Alexander SABOURET <asaboure@e1r8p11.clusters.42paris.fr>
2022-08-06 17:27:17 +0200

    fixed typo

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -6,8 +6,9 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     wget http://wordpress.org/latest.tar.gz -P /var/www/
     tar xfz /var/www/latest.tar.gz -C /var/www/
     rm -rf /var/www/html/*
-    mv /var/www/wordpress /var/www/html
+    mv /var/www/wordpress/* /var/www/html/
     rm -f /var/www/latest.tar.gz
+    rm -f /var/www/wordpress
     mv /var/www/wp-config.php /var/www/html/wp-config.php
     echo "done"
 fi
