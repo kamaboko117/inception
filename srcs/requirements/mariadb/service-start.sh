46a7621d27ee092c257b40d4b28eba02084e68dc
Alexander SABOURET <asaboure@e1r8p11.clusters.42paris.fr>
2022-08-06 19:04:48 +0200

    fixed typo?

--- srcs/requirements/mariadb/service-start.sh
+++ srcs/requirements/mariadb/service-start.sh
@@ -1,5 +1,5 @@
 service mysql start
 
 echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
-echo "GRANT ALL ON *.* TO 'kama'@'asaboure.42.fr' IDENTIFIED BY '123'" | mysql -u root
+echo "GRANT ALL ON *.* TO 'kama'@'asaboure.42.fr' IDENTIFIED BY '123';" | mysql -u root
 echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'asaboure.42.fr' IDENTIFIED BY 'password';" | mysql -u root
