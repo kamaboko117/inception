4c8e25ea3b0f579e0fb45121d65b64a6aa7bfb63
Alexander SABOURET <asaboure@e1r8p11.clusters.42paris.fr>
2022-08-06 19:24:39 +0200

    changed start script for db

--- srcs/requirements/mariadb/service-start.sh
+++ srcs/requirements/mariadb/service-start.sh
@@ -3,4 +3,3 @@ service mysql start
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_user'@'%' IDENTIFIED BY 'password';" | mysql -u root
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
