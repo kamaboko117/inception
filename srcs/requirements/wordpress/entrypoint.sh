6956e3a79e7ec1411dae4352eed5530966119df5
Alexander SABOURET <asaboure@e1r8p11.clusters.42paris.fr>
2022-08-06 16:41:01 +0200

    -

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -5,7 +5,7 @@ chown -R 755 /var/www/*
 if [ ! -f /var/www/html/wp-config.php ]; then
     wget http://wordpress.org/latest.tar.gz -P /var/www/
     tar xfz /var/www/latest.tar.gz -C /var/www/
-    mv /var/www/wordpress/* /var/www/html/
+    mv /var/www/wordpress /var/www/html
     rm -f /var/www/latest.tar.gz
     mv /var/www/wp-config.php /var/www/html/wp-config
 fi
