a8a7bed91cb9cd4278368aac05a4fa25bbc42e3c
Alexander SABOURET <asaboure@e1r8p11.clusters.42paris.fr>
2022-08-06 19:23:13 +0200

    changed start script for db

--- srcs/requirements/mariadb/service-start.sh
+++ srcs/requirements/mariadb/service-start.sh
@@ -1,5 +1,6 @@
 service mysql start
 
-echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
-echo "GRANT ALL ON *.* TO 'kama'@'asaboure.42.fr' IDENTIFIED BY '123';" | mysql -u root
-echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'asaboure.42.fr' IDENTIFIED BY 'password';" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_user'@'%' IDENTIFIED BY 'password';" | mysql -u root
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
