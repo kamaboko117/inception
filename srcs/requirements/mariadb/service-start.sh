4ed87821910580b52fa8880c5e5a678a8780e276
Alexander SABOURET <asaboure@e1r9p12.clusters.42paris.fr>
2022-08-08 17:52:08 +0200

    reverted 2 previous commits: obviously not working

--- srcs/requirements/mariadb/service-start.sh
+++ srcs/requirements/mariadb/service-start.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
+service mysql start
 
-if [ ! -f /var/lib/mysql/touched ]; then
-    service mysql start
-    echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root
-    echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PSWD';" | mysql -u root
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';" | mysql -u root
-    touch /var/lib/mysql/touched
-fi
-
-exec "$@"
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_user'@'%' IDENTIFIED BY 'password';" | mysql -u root
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';" | mysql -u root
