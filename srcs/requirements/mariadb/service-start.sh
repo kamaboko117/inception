4462bc9b2e3c78cf4e59209fbf2ea0877b1838a4
Alexander SABOURET <asaboure@e1r9p12.clusters.42paris.fr>
2022-08-08 17:42:05 +0200

    added check to prevent attempts at db creation if db already exists

--- srcs/requirements/mariadb/service-start.sh
+++ srcs/requirements/mariadb/service-start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-service mysql start
-
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PSWD';" | mysql -u root
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';" | mysql -u root
+if [ ! -f /var/lib/mysql/touched ]; then
+    service mysql start
+    echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root
+    echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PSWD';" | mysql -u root
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';" | mysql -u root
+    touch /var/lib/mysql/touched
+fi
 
 exec "$@"
