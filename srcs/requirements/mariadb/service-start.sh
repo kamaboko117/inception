ad3ad18e0614ca688b135433bd64f89a74b560d2
Alexander SABOURET <asaboure@e1r9p12.clusters.42paris.fr>
2022-08-08 17:35:40 +0200

    now running service-start.sh at entrypoint so that it can use environment passed by docker compose

--- srcs/requirements/mariadb/service-start.sh
+++ srcs/requirements/mariadb/service-start.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 service mysql start
 
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PSWD';" | mysql -u root
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';" | mysql -u root
+
+exec "$@"
